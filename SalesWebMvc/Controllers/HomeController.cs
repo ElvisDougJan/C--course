c0e22891244fb70f0f26ec19104807c90f48d389
unknown <edjelvis@gmail.com>
2018-08-26 16:12:13 -0300

    Conhecendo o Razor do ASP.NET

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -13,7 +13,8 @@ namespace SalesWebMvc.Controllers {
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "TESTE.";
+            ViewData["Teste"] = "Essa mensagem foi minha";
 
             return View();
         }
